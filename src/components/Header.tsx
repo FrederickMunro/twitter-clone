bdb94bb2c9b656c405ffd633dc12b120932fc8b9
FrederickMunro <fredmunro7@gmail.com>
2023-10-23 15:28:06 -0400

    Project initialization

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Header = () => {
+
+  return(
+    <Container>
+    </Container>
+  )
+}
+
+export default Header
+
+const Container = styled.div`
+  width: 100%;
+  height: 10%;
+`
