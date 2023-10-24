3854681fe6e272a90b6ba802ad418e54fa467974
FrederickMunro <fredmunro7@gmail.com>
2023-10-24 01:01:17 -0400

    Modified component to serve as a container for various middle tabs

--- src/components/Middle.tsx
+++ src/components/Middle.tsx
@@ -1,9 +1,17 @@
+import { ReactNode } from "react"
 import styled from "styled-components"
 
-const Middle = () => {
+interface MiddleProps {
+  title: string;
+  children: ReactNode;
+}
+
+const Middle = ({ title, children }: MiddleProps) => {
 
   return(
     <Container>
+      <H1>{title}</H1>
+      {children}
     </Container>
   )
 }
@@ -12,6 +20,10 @@ export default Middle
 
 const Container = styled.div`
   padding: 0 20px 0 20px;
-  min-width: 10px;
+  width: 600px;
   height: 100vh;
+`
+
+const H1 = styled.h1`
+  
 `
