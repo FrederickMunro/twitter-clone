b0061a552b461e09c87b488b6083081390d50e2e
FrederickMunro <fredmunro7@gmail.com>
2023-10-24 00:57:31 -0400

    Implemented navigation functionality to only rerender 1 components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,38 @@
 import { Routes, Route } from "react-router-dom";
+import styled from "styled-components";
 
-import Template from "./components/Template";
+import Left from "./components/Left";
+import Right from "./components/Right";
+import Messages from "./components/Messages";
+import MessagesTab from "./components/MessagesTab"
+import Home from "./components/Home";
+import Bookmarks from "./components/Bookmarks";
+import Explore from "./components/Explore";
+import Notifications from "./components/Notifications";
+import Profile from "./components/Profile";
 
 function App() {
 
 	return (
-		<>
+		<Container>
+			<Messages />
+			<Left />
 			<Routes>
-				<Route path='/' element={ <Template /> } />
+				<Route path='/home' element={ <Home /> } />
+				<Route path='/explore' element={ <Explore /> } />
+				<Route path='/notifications' element={ <Notifications /> } />
+				<Route path='/messages' element={ <MessagesTab /> } />
+				<Route path='/bookmarks' element={ <Bookmarks /> } />
+				<Route path='/Profile' element={ <Profile /> } />
 			</Routes>
-		</>
+      <Right />
+    </Container>
 	)
 }
 
 export default App
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`
