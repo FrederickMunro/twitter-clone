557b15808127448507e8a38f667257f8b06b6533
FrederickMunro <fredmunro7@gmail.com>
2023-10-24 00:14:51 -0400

    Menu item with icon and name

---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,46 @@
+import styled from "styled-components"
+
+interface MenuItemProps {
+  name: string;
+  icon: string;
+}
+
+const MenuItem = ({ name, icon }: MenuItemProps) => {
+  return(
+    <Button>
+      <Icon src={icon} />
+      <H3>{name}</H3>
+    </Button>
+  )
+}
+
+export default MenuItem
+
+const Button = styled.button`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+  min-height: 30px;
+  height: 10vh;
+  max-height: 50px;
+  padding: 9px 40px 11px 15px;
+  border-radius: 30px;
+  align-self: flex-start;
+  &:hover {
+    background-color: #111111;
+  }
+`
+
+const Icon = styled.img`
+  height: 100%;
+  padding-right: 30px;
+`
+
+const H3 = styled.h3`
+  font-size: 23px;
+  font-weight: lighter;
+  letter-spacing: 2px;
+`
