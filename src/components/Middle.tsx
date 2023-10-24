b67cfb5efd18d100324ea6cfb987bb61ac95b3cc
FrederickMunro <fredmunro7@gmail.com>
2023-10-24 00:15:21 -0400

    Created Middle component

---
+++ src/components/Middle.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+const Middle = () => {
+
+  return(
+    <Container>
+    </Container>
+  )
+}
+
+export default Middle
+
+const Container = styled.div`
+  padding: 0 20px 0 20px;
+  min-width: 10px;
+  height: 100vh;
+`
